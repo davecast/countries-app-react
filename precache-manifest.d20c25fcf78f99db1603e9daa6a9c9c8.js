c5e3dbda315b307fc250c667397efddd33bf5ffe
David Castillo <hola@davecastworks.com>
2020-06-06 16:55:29 -0400

    Updates

---
+++ precache-manifest.d20c25fcf78f99db1603e9daa6a9c9c8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32c326b55751ea014ab7d20d8134687",
+    "url": "/countries-app-react/index.html"
+  },
+  {
+    "revision": "ee9d49f459c51af7207a",
+    "url": "/countries-app-react/static/css/main.a02d6e4d.chunk.css"
+  },
+  {
+    "revision": "c1b6c0ae45d60a40d9ce",
+    "url": "/countries-app-react/static/js/2.b2571afa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/countries-app-react/static/js/2.b2571afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee9d49f459c51af7207a",
+    "url": "/countries-app-react/static/js/main.c236899c.chunk.js"
+  },
+  {
+    "revision": "0868ad498bbaf6b9e37f",
+    "url": "/countries-app-react/static/js/runtime-main.56401f43.js"
+  },
+  {
+    "revision": "3ef2b8e0dfd23ccae712e23d4effeb73",
+    "url": "/countries-app-react/static/media/icomoon.3ef2b8e0.woff"
+  },
+  {
+    "revision": "5300ffcab9e7b0c354bec03f031f0586",
+    "url": "/countries-app-react/static/media/icomoon.5300ffca.svg"
+  },
+  {
+    "revision": "544b321fb7a38bb33be460cb0c033abe",
+    "url": "/countries-app-react/static/media/icomoon.544b321f.eot"
+  },
+  {
+    "revision": "be551178f6ff2ab22cc60a1c2922876b",
+    "url": "/countries-app-react/static/media/icomoon.be551178.ttf"
+  }
+]);
